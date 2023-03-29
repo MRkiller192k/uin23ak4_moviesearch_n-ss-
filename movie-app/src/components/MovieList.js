ff33193b7f9c6d3f9a3c2359bf3b233225c321b0
MRkiller192k <mikkelnaess08@gmail.com>
2023-03-30 00:21:04 +0200

    laget en branch

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -8,8 +8,11 @@ const MovieList = (props) => {
         <div className="d-flex justify-content-start m-3"  style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}}>
             <h2>{movie.Title}</h2>
             <img src={movie.Poster} alt='movie'></img>
-            <Link to={`movie/${movie.imdbID}`}>Les mer</Link>
-           
+            <li>
+                <p>year: {movie.Year}</p>
+                <p>Type: {movie.Type}</p>
+                <Link to={`movie/${movie.imdbID}`}>Les mer</Link>
+           </li>
         </div >
         ))}
         </>
